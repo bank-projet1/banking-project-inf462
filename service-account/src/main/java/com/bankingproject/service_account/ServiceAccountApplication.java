4af3a8d45975e74eebf93a6b84f3edab9cd76632
dradone237 <dradonetakougang70@gmail.com>
2026-06-11 16:31:17 +0100

    Initial microservices platform base

---
+++ service-account/src/main/java/com/bankingproject/service_account/ServiceAccountApplication.java
@@ -0,0 +1,12 @@
+package com.bankingproject.service_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAccountApplication.class, args);
+	}
+}
