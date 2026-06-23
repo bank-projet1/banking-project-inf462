05998d5ff84efd88e10acb0d2f36cbea0ae0c508
dradone237 <dradonetakougang70@gmail.com>
2026-06-23 15:18:50 +0100

    Reapply "Implement customer and account management services"
    This reverts commit 734e14bdbf3771828307995c743e17c60552fb12.

---
+++ service-customer/src/main/java/com/bankingproject/service_customer/ServiceCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.bankingproject.service_customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCustomerApplication.class, args);
+	}
+
+}
